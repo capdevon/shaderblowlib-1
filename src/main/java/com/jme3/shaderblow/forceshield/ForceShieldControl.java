bc39081d17244ad4a7d5247c326e6d8c973d54ce
Wyatt Gillette <jefferydeaver2010@gmail.com>
2022-01-07 18:40:40 +0100

    Update ForceShieldControl.java

--- src/main/java/com/jme3/shaderblow/forceshield/ForceShieldControl.java
+++ src/main/java/com/jme3/shaderblow/forceshield/ForceShieldControl.java
@@ -1,10 +1,7 @@
 package com.jme3.shaderblow.forceshield;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-import com.jme3.export.JmeExporter;
-import com.jme3.export.JmeImporter;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -12,7 +9,7 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
 import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.control.Control;
+import com.jme3.scene.control.AbstractControl;
 import com.jme3.shader.VarType;
 import com.jme3.texture.Texture;
 
@@ -20,48 +17,40 @@ import com.jme3.texture.Texture;
  *
  * @author shaderblow
  */
-public class ForceShieldControl implements Control {
+public class ForceShieldControl extends AbstractControl {
 
     private final Material material;
-    private float maxTime;
-    private final ArrayList<Vector3f> collisions;
-    private final ArrayList<Float> collisionTimes;
-    private Spatial model;
+    private final ArrayList<Vector3f> collisions = new ArrayList<>();
+    private final ArrayList<Float> collisionTimes = new ArrayList<>();
     private boolean numChanged;
-    private boolean enabled;
     private boolean work;
-    private float timer;
-    private float timerSize;
-    private final int MAX_HITS;
+    private float timer = 0;
+    private float maxTime = 0.4f;
+    private float timerSize = maxTime * 3f;
+    private final int MAX_HITS = 4;
 
+    /**
+     * Create a ForceShieldControl instance.
+     *
+     * @param material
+     */
     public ForceShieldControl(Material material) {
         this.material = material;
-        numChanged = false;
-        enabled = true;
-        work = false;
-        maxTime = 0.4f;
-        timerSize = maxTime * 3f;
-        collisionTimes = new ArrayList<>();
-        collisions = new ArrayList<>();
-        MAX_HITS = 4;
-        timer = 0;
     }
 
     public void registerHit(Vector3f position) {
-        if (!enabled) {
-            return;
+        if (isEnabled()) {
+            timer = 0f;
+            material.setBoolean("Work", true);
+            work = true;
+
+            Vector3f localPos = new Vector3f();
+            spatial.worldToLocal(position, localPos);
+            collisions.add(new Vector3f(localPos.x, localPos.y, localPos.z));
+            collisionTimes.add(maxTime);
+            numChanged = true;
+            updateCollisionPoints();
         }
-
-        timer = 0f;
-        material.setBoolean("Work", true);
-        work = true;
-
-        Vector3f lposition = new Vector3f();
-        model.worldToLocal(position.clone(), lposition);
-        collisions.add(new Vector3f(lposition.x, lposition.y, lposition.z));
-        collisionTimes.add(maxTime);
-        numChanged = true;
-        updateCollisionPoints();
     }
 
     public void setColor(ColorRGBA color) {
@@ -76,29 +65,8 @@ public class ForceShieldControl implements Control {
         material.setTexture("ColorMap", texture);
     }
 
-    public Material getMaterial() {
-        return material;
-    }
-
     public void setEffectSize(float size) {
-        material.setFloat("MaxDistance", size / model.getLocalScale().x);
-    }
-
-    protected void updateCollisionAlpha() {
-        float[] alphas = new float[Math.min(collisionTimes.size(), MAX_HITS)];
-        for (int i = 0; i < alphas.length && i < MAX_HITS; i++) {
-            alphas[i] = collisionTimes.get(collisions.size() - 1 - i) / maxTime;
-        }
-        material.setParam("CollisionAlphas", VarType.FloatArray, alphas);
-    }
-
-    protected void updateCollisionPoints() {
-        Vector3f[] collisionsArray = new Vector3f[Math.min(collisions.size(), MAX_HITS)];
-        for (int i = 0; i < collisions.size() && i < MAX_HITS; i++) {
-            collisionsArray[i] = collisions.get(collisions.size() - 1 - i);
-        }
-        material.setParam("Collisions", VarType.Vector3Array, collisionsArray);
-        //material.setInt("CollisionNum", Math.min(collisions.size(), MAX_HITS));
+        material.setFloat("MaxDistance", size / spatial.getLocalScale().x);
     }
 
     public float getMaxTime() {
@@ -111,15 +79,17 @@ public class ForceShieldControl implements Control {
     }
 
     @Override
-    public void setSpatial(Spatial model) {
-        this.model = model;
-        model.setMaterial(material);
-        model.setQueueBucket(Bucket.Transparent);
+    public void setSpatial(Spatial sp) {
+        super.setSpatial(sp);
+        if (spatial != null) {
+            spatial.setMaterial(material);
+            spatial.setQueueBucket(Bucket.Transparent);
+        }
     }
 
     @Override
-    public void update(float tpf) {
-        if (work && enabled) {
+    public void controlUpdate(float tpf) {
+        if (work) {
 
             if (timer > timerSize) {
                 collisions.clear();
@@ -155,32 +125,25 @@ public class ForceShieldControl implements Control {
     }
 
     @Override
-    public void render(RenderManager rm, ViewPort vp) {
+    public void controlRender(RenderManager rm, ViewPort vp) {
         // TODO Auto-generated method stub
     }
 
-    @Override
-    public void read(JmeImporter arg0) throws IOException {
-        // TODO Auto-generated method stub
+    protected void updateCollisionAlpha() {
+        float[] alphas = new float[Math.min(collisionTimes.size(), MAX_HITS)];
+        for (int i = 0; i < alphas.length && i < MAX_HITS; i++) {
+            alphas[i] = collisionTimes.get(collisions.size() - 1 - i) / maxTime;
+        }
+        material.setParam("CollisionAlphas", VarType.FloatArray, alphas);
     }
 
-    @Override
-    public void write(JmeExporter arg0) throws IOException {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public Control cloneForSpatial(Spatial spatial) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
+    protected void updateCollisionPoints() {
+        Vector3f[] collisionsArray = new Vector3f[Math.min(collisions.size(), MAX_HITS)];
+        for (int i = 0; i < collisions.size() && i < MAX_HITS; i++) {
+            collisionsArray[i] = collisions.get(collisions.size() - 1 - i);
+        }
+        material.setParam("Collisions", VarType.Vector3Array, collisionsArray);
+        //material.setInt("CollisionNum", Math.min(collisions.size(), MAX_HITS));
     }
 
 }
