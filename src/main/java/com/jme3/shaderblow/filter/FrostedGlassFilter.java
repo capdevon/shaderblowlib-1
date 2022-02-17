81a25eabe18eb1658f2fa91a5db0966eb56e3996
Wyatt Gillette <jefferydeaver2010@gmail.com>
2022-02-17 14:31:31 +0100

    Update FrostedGlassFilter.java

--- src/main/java/com/jme3/shaderblow/filter/FrostedGlassFilter.java
+++ src/main/java/com/jme3/shaderblow/filter/FrostedGlassFilter.java
@@ -39,7 +39,6 @@ import com.jme3.export.JmeExporter;
 import com.jme3.export.JmeImporter;
 import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
