28614db7be8bdf8d3d82eda30cf2a66dcb957943
Wyatt Gillette <jefferydeaver2010@gmail.com>
2022-02-17 14:24:49 +0100

    Update FrostedGlassFilter.java

--- src/main/java/com/jme3/shaderblow/filter/FrostedGlassFilter.java
+++ src/main/java/com/jme3/shaderblow/filter/FrostedGlassFilter.java
@@ -31,8 +31,15 @@
  */
 package com.jme3.shaderblow.filter;
 
+import java.io.IOException;
+
 import com.jme3.asset.AssetManager;
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.post.Filter;
 import com.jme3.renderer.RenderManager;
 import com.jme3.renderer.ViewPort;
@@ -70,6 +77,9 @@ public class FrostedGlassFilter extends Filter {
 
     public void setRandomScale(float randomScale) {
         this.randomScale = randomScale;
+        if (material != null) {
+            material.setFloat("RandomScale", randomScale);
+        }
     }
 
     public float getRandomFactor() {
@@ -78,5 +88,24 @@ public class FrostedGlassFilter extends Filter {
 
     public void setRandomFactor(float randomFactor) {
         this.randomFactor = randomFactor;
+        if (material != null) {
+            material.setFloat("RandomFactor", randomFactor);
+        }
+    }
+
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        super.write(ex);
+        OutputCapsule oc = ex.getCapsule(this);
+        oc.write(randomScale, "RandomScale", 2.1f);
+        oc.write(randomFactor, "RandomFactor", 0.025f);
+    }
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        super.read(im);
+        InputCapsule ic = im.getCapsule(this);
+        randomScale = ic.readFloat("RandomScale", 2.1f);
+        randomFactor = ic.readFloat("RandomFactor", 0.025f);
     }
 }
